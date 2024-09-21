870a09e1141c210e98c131116a2d27d4a4e5bf57
Leandro Arturi <lea.arturi@gmail.com>
2024-09-21 11:33:30 -0300

    Report hello

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
+import { Response } from 'express';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
 
   @Get()
-  async hello() {
-    return await this.basicReportsService.hello();
+  async hello(@Res() response: Response) {
+    const pdfDoc = await this.basicReportsService.hello();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    response.setHeader(
+      'Content-Disposition',
+      'attachment; filename=filename.pdf',
+    );
+
+    pdfDoc.info.Title = 'Hello-PDF.pdf';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
   }
 }
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrinterService } from '../printer/printer.service';
+import { getHelloWorldReport } from '../reports/hello-world.report';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -7,7 +9,15 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
+  constructor(private readonly printerService: PrinterService) {
+    super();
+  }
+
   async hello() {
-    return this.employees.findMany();
+    const docDefinition = getHelloWorldReport({ name: 'Leandro' });
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
   }
 }
