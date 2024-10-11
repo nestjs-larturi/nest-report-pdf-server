b04c353fd9a85cda8ffdac49770f6803c4899332
Leandro Arturi <lea.arturi@gmail.com>
2024-10-11 18:16:37 -0300

    QR y tabla

--- src/reports/order-by-id.report.ts
+++ src/reports/order-by-id.report.ts
@@ -3,7 +3,8 @@ import {
   StyleDictionary,
   TDocumentDefinitions,
 } from 'pdfmake/interfaces';
-import { DataFormatter } from 'src/helpers';
+import { CurrencyFormatter, DataFormatter } from 'src/helpers';
+import { footerSection } from './sections/footer.section';
 
 const logo: Content = {
   image: 'src/assets/tucan-banner.png',
@@ -25,19 +26,117 @@ export const orderByIdReport = (): TDocumentDefinitions => {
     styles: styles,
     header: logo,
     pageMargins: [37, 60, 40, 60],
+    footer: footerSection,
     content: [
+      // Header
       {
-        text: 'Tucan Code',
+        text: 'Tucan Code\n\n',
         style: 'header',
       },
+      // Address and Recipe
       {
         columns: [
           {
-            text: '15 Montgomery Str, Suite 100 \nOttawa ON K2Y 9X1, CANADA\n BN: 12783671823 \nhttps://devtalles.com',
+            text: '15 Montgomery Str, Suite 100 \nOttawa ON K2Y 9X1, CANADA\n BN: 12783671823 \nhttps://tucancode.com',
           },
           {
-            text: `Recibo No#: 10255\nFecha del recibo: ${DataFormatter.getDDMMMMYYYY(new Date())} \nPagar antes de: ${DataFormatter.getDDMMMMYYYY(new Date())}`,
+            text: [
+              {
+                text: 'Recibo No#: 10255\n',
+                bold: true,
+                style: { fontSize: 14 },
+              },
+              `Fecha del recibo: ${DataFormatter.getDDMMMMYYYY(new Date())} \nPagar antes de: ${DataFormatter.getDDMMMMYYYY(new Date())}`,
+            ],
             alignment: 'right',
+          },
+        ],
+      },
+      // QR
+      { qr: 'https://leandroarturi.com.ar', fit: 75, alignment: 'right' },
+      // Client address
+      {
+        text: [
+          {
+            text: `Cobrar a:\nRazón Social: `,
+          },
+          {
+            text: `Juan Perez\n Greenvile 1123`,
+            bold: true,
+          },
+        ],
+      },
+      // Order details table
+      {
+        layout: 'headerLineOnly',
+        margin: [0, 23],
+        table: {
+          headerRows: 1,
+          widths: [50, '*', 'auto', 'auto', 'auto'],
+          body: [
+            ['Id', 'Descripcion', 'Cantidad', 'Precio', 'Total'],
+            [
+              '1',
+              'Producto 1',
+              '4',
+              'Precio',
+              {
+                text: CurrencyFormatter.formatCurrency(100),
+                alignment: 'right',
+              },
+            ],
+            [
+              '1',
+              'Producto 1',
+              '4',
+              'Precio',
+              {
+                text: CurrencyFormatter.formatCurrency(100),
+                alignment: 'right',
+              },
+            ],
+            [
+              '1',
+              'Producto 1',
+              '4',
+              'Precio',
+              {
+                text: CurrencyFormatter.formatCurrency(100),
+                alignment: 'right',
+              },
+            ],
+          ],
+        },
+      },
+      // Totals
+      {
+        columns: [
+          {
+            width: '*',
+            text: '',
+          },
+          {
+            width: 'auto',
+            layout: 'noBorders',
+            table: {
+              body: [
+                [
+                  'Subtotal',
+                  {
+                    text: CurrencyFormatter.formatCurrency(122),
+                    alignment: 'right',
+                  },
+                ],
+                [
+                  { text: 'Total', bold: true },
+                  {
+                    text: CurrencyFormatter.formatCurrency(160),
+                    alignment: 'right',
+                    bold: true,
+                  },
+                ],
+              ],
+            },
           },
         ],
       },
