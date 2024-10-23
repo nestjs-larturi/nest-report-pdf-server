6a634cbcd72afc646a8aaf58910d1a8449dc7d57
Leandro Arturi <lea.arturi@gmail.com>
2024-10-23 18:56:06 -0300

    Finalizando curso
    Author:    Leandro Arturi <lea.arturi@gmail.com.com>

--- src/extra-reports/extra-reports.controller.ts
+++ src/extra-reports/extra-reports.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { ExtraReportsService } from './extra-reports.service';
 import { Response } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('extra-reports')
 export class ExtraReportsController {
   constructor(private readonly extraReportsService: ExtraReportsService) {}
 
   @Get('html-report')
+  @ApiTags('extra-reports') // Swagger
+  @ApiOperation({ summary: 'Reporte html' }) // Swagger
+  @ApiResponse({ status: 200, description: 'Reporte html ejemplo' }) // Swagger
   async getHtmlReport(@Res() response: Response) {
     const pdfDoc = this.extraReportsService.getHtmlReport();
 
@@ -16,7 +20,38 @@ export class ExtraReportsController {
       'attachment; filename=filename.pdf',
     );
 
-    pdfDoc.info.Title = 'Hello-PDF.pdf';
+    pdfDoc.info.Title = 'HTML-PDF.pdf';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('html-billing-report')
+  @ApiTags('extra-reports') // Swagger
+  @ApiOperation({ summary: 'Reporte html de una factura' }) // Swagger
+  @ApiResponse({ status: 200, description: 'Reporte html de una factura' }) // Swagger
+  async getBillingReport(@Res() response: Response) {
+    const pdfDoc = this.extraReportsService.getBillingReport();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    response.setHeader(
+      'Content-Disposition',
+      'attachment; filename=filename.pdf',
+    );
+
+    pdfDoc.info.Title = 'Recipe-PDF.pdf';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('custom-size')
+  @ApiTags('extra-reports') // Swagger
+  @ApiOperation({ summary: 'Custom size' }) // Swagger
+  @ApiResponse({ status: 200, description: 'Custom size' }) // Swagger
+  async getCustomSize(@Res() response: Response) {
+    const pdfDoc = this.extraReportsService.getCustomSize();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Custom-Size';
     pdfDoc.pipe(response);
     pdfDoc.end();
   }
--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -5,14 +5,18 @@ import { PrinterService } from '../printer/printer.service';
 import { getHtmlContent } from 'src/helpers';
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.section';
+import { billingReport } from 'src/reports';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
 
   getHtmlReport() {
-    const html = fs.readFileSync('src/reports/html/basic-html.html', 'utf-8');
-    const content = getHtmlContent(html);
+    const html = fs.readFileSync('src/reports/html/basic3.html', 'utf-8');
+    const content = getHtmlContent(html, {
+      client: 'Leandro Arturi',
+    });
     const docDefinition: TDocumentDefinitions = {
       pageMargins: [40, 110, 40, 60],
       header: headerSection({
@@ -20,9 +24,42 @@ export class ExtraReportsService {
         subTitle: 'Convertir HTML a PDF con pdfmake',
       }),
       content: content,
+      footer: footerSection,
     };
 
     const doc = this.printerService.createPdf(docDefinition);
+    return doc;
+  }
+
+  getBillingReport() {
+    const docDefinition = billingReport();
+
+    const doc = this.printerService.createPdf(docDefinition);
+    return doc;
+  }
+
+  getCustomSize() {
+    const doc = this.printerService.createPdf({
+      pageSize: 'A4',
+      // pageSize: {
+      //   width: 150,
+      //   height: 300,
+      // },
+      content: [
+        {
+          qr: 'https://leandroarturi.com.ar',
+          fit: 100,
+          alignment: 'center',
+        },
+        {
+          text: 'Reporte con tamaño',
+          fontSize: 10,
+          alignment: 'center',
+          margin: [0, 20],
+        },
+      ],
+    });
+
     return doc;
   }
 }
